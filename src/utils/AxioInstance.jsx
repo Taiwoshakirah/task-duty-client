273249e9d8f987004569aa9aae57566ea4413f5d
Taiwoshakirah <shakirattaiwo6@gmail.com>
2025-04-07 08:53:53 -0700

    chaging the base url to render

--- src/utils/AxioInstance.jsx
+++ src/utils/AxioInstance.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const axiosInstance = axios.create({baseURL:"http://localhost:3006"})
+const axiosInstance = axios.create({baseURL:"https://task-duty-server-0wq1.onrender.com"})
 
 export default axiosInstance
